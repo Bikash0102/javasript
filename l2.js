85a63f36dcfd315ba03f1e0fa651bd61a58e4d27
Bikash <bbbhowmik04@gmial.com>
2023-08-03 01:30:29 +0530

    day2

---
+++ l2.js
@@ -0,0 +1,58 @@
+let firstname="bikash";
+let winningnumber=19;
+// let userinput=prompt("guess anumber");
+// // take string input 
+// userinput= +userinput;
+// if(userinput===winningnumber)
+// {
+//     alert("your guess is right");
+// }
+// else
+// {
+//     if(userinput<winningnumber)
+//     {
+//         userinput=prompt("enter higher number");
+//     }
+//     else
+//     {
+//         userinput=prompt("enter lower  number");
+
+//     }
+// }
+
+let i=1;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
+let num=10;
+let total=0;
+i=1;
+while(i<=num)
+{
+    total+=i;
+    i++;
+}
+console.log(total);
+let a=0;
+for(let i=0;i<=num;i++)
+{
+    a+=i;
+
+}
+console.log(a);
+let fruit=["apple","mango","grapes"];
+console.log(fruit);
+console.log(fruit[1]);
+let number=[1,2,3,4];
+console.log(number);
+console.log(number[1]);
+fruit[1]="banana";
+console.log(fruit);
+let obj={};
+// object literal 
+fruit.push("lannd");
+
+
+
